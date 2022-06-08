d6293a7f556e52d61643c54716a2dc1c10e6e354
sidneyrdm <sidneybritomal@gmail.com>
2022-06-07 22:31:29 -0300

    implementação dos métodos updateCliente e deleteById

--- src/main/java/br/com/githubsidneyrdm/clientes/rest/ClienteController.java
+++ src/main/java/br/com/githubsidneyrdm/clientes/rest/ClienteController.java
@@ -29,4 +29,28 @@ public class ClienteController {
         return clienteRepository.findById(id)
                 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Integer id){
+        //clienteRepository.deleteById(id); mais utilizado
+
+        clienteRepository.findById(id)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCliente(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
+        clienteRepository.findById(id)
+                .map(cliente -> {
+                    clienteAtualizado.setId(cliente.getId());
+                    return clienteRepository.save(clienteAtualizado);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
